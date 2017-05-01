1c5a5350729a8b65c35df61cdfcbe5979d627927
Dorian McDougald <demcdougald@gmail.com>
2017-05-01 02:34:33 -0400

    Updated scripts and added requirements file

--- testing.py
+++ testing.py
@@ -1,3 +1,15 @@
-print "Content-Type: text/plain"
-print ""
-print "Congratulations, it's a web app!"
+import logging
+from flask import flask
+
+app = Flask(_name_)
+
+# print "Content-Type: text/plain"
+# print ""
+# print "Congratulations, it's a web app!"
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+if __name__ == '__testing__':
+    app.run(host='127.0.0.1',port=8080,debug=True)
